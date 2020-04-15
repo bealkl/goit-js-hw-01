6e1f46c18ffa9ca42183f9ce012ca68933a60f75
Oleksandr Bevzytskyi <bealkl@gmail.com>
2020-04-15 15:54:19 +0300

    task 2 done

--- task-2.js
+++ task-2.js
@@ -1,18 +1,18 @@
 {
   const total = 100;
 
-  //  check 001
+  // check 001
   let ordered = 20;
 
   if (ordered < total) console.log('На складе недостаточно твоаров!');
   else console.log('Заказ оформлен, с вами свяжется менеджер');
 
-  //  check 002
+  // check 002
   ordered = 80;
   if (ordered < total) console.log('На складе недостаточно твоаров!');
   else console.log('Заказ оформлен, с вами свяжется менеджер');
 
-  //  check 003
+  // check 003
   ordered = 130;
   if (ordered < total) console.log('На складе недостаточно твоаров!');
   else console.log('Заказ оформлен, с вами свяжется менеджер');
