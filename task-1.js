f4efcba3fa2d9c4d3ba7050cdd46b0d6f561565a
Oleksandr Bevzytskyi <bealkl@gmail.com>
2020-04-15 15:52:45 +0300

    task 1 done

---
+++ task-1.js
@@ -0,0 +1,10 @@
+{
+  let name = 'Генератор защитного поля';
+  let price = 1000;
+  let formOut = 'Выбран «' + name + '», цена за штуку ' + price + ' кредитов';
+
+  console.log(formOut);
+
+  price = 2000;
+  console.log(formOut);
+}
---
+++ task-2.js
@@ -0,0 +1,19 @@
+{
+  const total = 100;
+
+  // check 001
+  let ordered = 20;
+
+  if (ordered < total) console.log('На складе недостаточно твоаров!');
+  else console.log('Заказ оформлен, с вами свяжется менеджер');
+
+  // check 002
+  ordered = 80;
+  if (ordered < total) console.log('На складе недостаточно твоаров!');
+  else console.log('Заказ оформлен, с вами свяжется менеджер');
+
+  // check 003
+  ordered = 130;
+  if (ordered < total) console.log('На складе недостаточно твоаров!');
+  else console.log('Заказ оформлен, с вами свяжется менеджер');
+}
